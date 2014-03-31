00394d008b73a983375b849ecb4afb03a6a4b077
C622 <christian.622@gmail.com>
2014-03-31 16:00:59 +0200

    Added more info lines

--- ustatus.sh
+++ ustatus.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-btc list | grep -E '"name"|"progress"|"state"' | sed -e 's/^.*"name": "\(.*\)".*$/Name      : \1/' -e 's/^.*"progress": \(.*.[0-9]\).*$/Progress  : \1 %/' -e 's/^.*"state": "\(.*\)".*$/State     : \1#/' | tr '#' '\n'
+btc list | \
+grep -E '"name"|"progress"|"state"|"size"|"order"' | \
+sed -e 's/^.*"name": "\(.*\)".*$/Name      : \1/' | \
+sed -e 's/^.*"progress": \(.*.[0-9]\).*$/Progress  : \1 %/' | \
+sed -e 's/^.*"size": \([0-9]*\).*$/Size      : \1/' | \
+sed -e 's/^.*"state": "\(.*\)".*$/State     : \1#/' | \
+sed -e 's/^.*"order": \([0-9]*\).*$/Order     : \1/' | \
+tr '#' '\n'
